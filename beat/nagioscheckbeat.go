e52261b674538a49fac097214191449d011c9c93
PhaedrusTheGreek <ccie11021@gmail.com>
2016-01-07 11:25:15 -0500

    Added module for metricbeat

--- beat/nagioscheckbeat.go
+++ beat/nagioscheckbeat.go
@@ -2,11 +2,11 @@ package beat
 
 import (
 	"github.com/PhaedrusTheGreek/nagioscheckbeat/nagiosperf"
-	"github.com/elastic/libbeat/beat"
-	"github.com/elastic/libbeat/cfgfile"
-	"github.com/elastic/libbeat/common"
-	"github.com/elastic/libbeat/logp"
-	"github.com/elastic/libbeat/publisher"
+	"github.com/elastic/beats/libbeat/beat"
+	"github.com/elastic/beats/libbeat/cfgfile"
+	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/logp"
+	"github.com/elastic/beats/libbeat/publisher"
 	"github.com/mattn/go-shellwords"
 	"os/exec"
 	"strings"
@@ -74,87 +74,8 @@ func (nagiosCheck *NagiosCheckBeat) Run(b *beat.Beat) error {
 
 		for _, check := range nagiosCheck.checks {
 
-			startTime := time.Now()
-			startMs := startTime.UnixNano() / int64(time.Millisecond)
-
-			args := ""
-			if check.Args != nil { 
-			  	args = *check.Args
-			}
-
-			check_event := common.MapStr{
-				"@timestamp": common.Time(startTime),
-				"type":       "nagioscheck",
-				"cmd":        *check.Cmd,
-				"args":       args,
-			}
-
-
-			logp.Debug("nagioscheck", "Running Command: %q", *check.Cmd)
-
-			//arg_fields := strings.Fields(args)
-			arg_fields, err := shellwords.Parse(args) // Smarter
-
-			if err != nil {
-				logp.Err("Could not parse args %q", args)
-			}
-
-			cmd := exec.Command(*check.Cmd, arg_fields...)
-			var waitStatus syscall.WaitStatus
-
-			/* Go will return 'err' if the command exits abnormally (non-zero return code).
-			Nagios commands always will exit abnormally when a check fails, so from
-			a funcational perspective, this doesn't help us.  Instead, if the ProcessState is nil,
-			that tells us that the command coulnd't run for some reason, which does help.
-			*/
-
-			output, err := cmd.CombinedOutput()
-			if cmd.ProcessState == nil {
-				logp.Err("Command Error: %v", err)
-				continue
-			}
-			waitStatus = cmd.ProcessState.Sys().(syscall.WaitStatus)
-
-			logp.Debug("nagioscheck", "Command Returned: %q, exit code %d", output, waitStatus.ExitStatus())
-
-			parts := strings.Split(string(output), "|")
-			check_event["message"] = parts[0]
-			check_event["status"] = nagiosperf.NiceStatus(waitStatus.ExitStatus())
-			check_event["took_ms"] = time.Now().UnixNano()/int64(time.Millisecond) - startMs
-
-			// publish the check result, even if there is no perf data
-			nagiosCheck.events.PublishEvent(check_event)
-
-			if len(parts) > 1 {
-				logp.Debug("nagioscheck", "Parsing: %q", parts[1])
-				perfs, errors := nagiosperf.ParsePerfString(parts[1])
-				if len(errors) > 0 {
-					for _, err := range errors {
-						logp.Err("Command Error: %v", err)
-					}
-				} else {
-
-					logp.Debug("nagioscheck", "Command Returned '%d' Perf Metrics: %v", len(perfs), perfs)
-
-					for _, perf := range perfs {
-
-						metric_event := common.MapStr{
-						 "@timestamp" : common.Time(startTime),
-						 "type"       : "nagiosmetric",
-						 "name"       :	*check.Name,
-						 "label"      :	perf.Label,
-						 "uom"        :	perf.Uom,
-						 "value"      : perf.Value,
-						 "min"        : perf.Min,
-						 "max"        : perf.Max,
-						 "warning"    : perf.Warning,
-						 "critical"   : perf.Critical,
-						}
-
-						nagiosCheck.events.PublishEvent(metric_event)
-
-					}
-				}
+			for _, event := range Check(&check) {
+				nagiosCheck.events.PublishEvent(event)
 			}
 
 		}
@@ -164,6 +85,108 @@ func (nagiosCheck *NagiosCheckBeat) Run(b *beat.Beat) error {
 	}
 
 	return nil
+}
+
+func Check(check *NagiosCheckConfig) (events []common.MapStr) {
+
+	startTime := time.Now()
+	startMs := startTime.UnixNano() / int64(time.Millisecond)
+
+	if check == nil {
+		logp.Err("Invalid/Missing Nagios Check Configuration")
+		return
+	}
+
+	if check.Name == nil {
+		logp.Err("Must Specify a Nagios Check Name")
+		return
+	}
+
+	if check.Cmd == nil {
+		logp.Err("Must Specify a Nagios Check Command")
+		return
+	}
+
+	args := ""
+	if check.Args != nil {
+		args = *check.Args
+	}
+
+	check_event := common.MapStr{
+		"@timestamp": common.Time(startTime),
+		"type":       "nagioscheck",
+		"cmd":        *check.Cmd,
+		"args":       args,
+	}
+
+	logp.Debug("nagioscheck", "Running Command: %q", *check.Cmd)
+
+	//arg_fields := strings.Fields(args)
+	arg_fields, err := shellwords.Parse(args) // Smarter
+
+	if err != nil {
+		logp.Err("Could not parse args %q", args)
+	}
+
+	cmd := exec.Command(*check.Cmd, arg_fields...)
+	var waitStatus syscall.WaitStatus
+
+	/* Go will return 'err' if the command exits abnormally (non-zero return code).
+	Nagios commands always will exit abnormally when a check fails, so from
+	a funcational perspective, this doesn't help us.  Instead, if the ProcessState is nil,
+	that tells us that the command coulnd't run for some reason, which does help.
+	*/
+
+	output, err := cmd.CombinedOutput()
+	if cmd.ProcessState == nil {
+		logp.Err("Command Error: %v", err)
+		return
+	}
+	waitStatus = cmd.ProcessState.Sys().(syscall.WaitStatus)
+
+	logp.Debug("nagioscheck", "Command Returned: %q, exit code %d", output, waitStatus.ExitStatus())
+
+	parts := strings.Split(string(output), "|")
+	check_event["message"] = parts[0]
+	check_event["status"] = nagiosperf.NiceStatus(waitStatus.ExitStatus())
+	check_event["took_ms"] = time.Now().UnixNano()/int64(time.Millisecond) - startMs
+
+	// publish the check result, even if there is no perf data
+	events = append(events, check_event)
+
+	if len(parts) > 1 {
+		logp.Debug("nagioscheck", "Parsing: %q", parts[1])
+		perfs, errors := nagiosperf.ParsePerfString(parts[1])
+		if len(errors) > 0 {
+			for _, err := range errors {
+				logp.Err("Command Error: %v", err)
+			}
+		} else {
+
+			logp.Debug("nagioscheck", "Command Returned '%d' Perf Metrics: %v", len(perfs), perfs)
+
+			for _, perf := range perfs {
+
+				metric_event := common.MapStr{
+					"@timestamp": common.Time(startTime),
+					"type":       "nagiosmetric",
+					"name":       *check.Name,
+					"label":      perf.Label,
+					"uom":        perf.Uom,
+					"value":      perf.Value,
+					"min":        perf.Min,
+					"max":        perf.Max,
+					"warning":    perf.Warning,
+					"critical":   perf.Critical,
+				}
+
+				events = append(events, metric_event)
+
+			}
+		}
+	}
+
+	return
 }
 
 func (nagiosCheck *NagiosCheckBeat) Cleanup(b *beat.Beat) error {
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	nagioscheckbeat "github.com/PhaedrusTheGreek/nagioscheckbeat/beat"
-	"github.com/elastic/libbeat/beat"
+	"github.com/elastic/beats/libbeat/beat"
 )
 
 func main() {
