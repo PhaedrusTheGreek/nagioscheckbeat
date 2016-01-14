2abd15d58ab4e285320a0c92460b7aa4cdcf4190
PhaedrusTheGreek <ccie11021@gmail.com>
2016-01-14 13:50:14 -0500

    added watch for blog

--- blog/elasticsearch/config/scripts/monitor-hosts.groovy
+++ blog/elasticsearch/config/scripts/monitor-hosts.groovy
@@ -1,1 +1,11 @@
-ctx.payload.hits.total < 3
+def minutes = 5
+def now = DateTime.now().getMillis()
+ctx.vars.hosts = [ up: [], down: [] ]
+ctx.payload.aggregations.hosts.buckets.each {
+ def last_heartbeat = it.latest_heartbeat.hits.hits[0].sort[0];
+ def ms_ago = now - last_heartbeat
+ if (ms_ago > (minutes * 1000) ){
+   ctx.vars.hosts.down.add( [ hostname: it.key, last_heartbeat: new Date(last_heartbeat) ])
+ } 
+}
+return ctx.vars.hosts.down.size() > 0
